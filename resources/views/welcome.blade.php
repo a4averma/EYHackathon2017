564860c01c26154db83362b7f3473e8c3047edbb
Aditya Verma <aditya_verma@outlook.com>
2017-11-06 09:19:48 +0530

    Some UI Fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,13 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <style>
+            body {
+                font-family: 'Raleway', sans-serif;
+            }
+        </style>
     </head>
     <body>
         <nav class="navbar sticky-top navbar-dark bg-primary justify-content-around">
@@ -19,7 +25,7 @@
                 <i class="fa fa-check-circle" aria-hidden="true"></i>
                 Compliance Monitor
             </a>
-            <a href="/refresh" class="btn btn-sm btn-outline-light my-2">Refresh</a>
+            <a href="/refresh" class="btn btn-sm btn-outline-light my-2"><i class="fa fa-retweet" aria-hidden="true"></i> Reload</a>
             <form class="form-inline" method="post" action="{{ route('search') }}">
                 <input class="form-control mr-sm-2" name="q" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
@@ -27,22 +33,32 @@
             </form>
         </nav>
 
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                @if(!empty($query))
+        <div>
+            @if(!empty($query))
+            <div class="jumbotron">
+                <div class="container">
+
                     <h1 class="display-3">Keyword: {{$query}}</h1>
                     <br>
                     <br>
                     <br>
                     <br>
-                @endif
+                </div>
+            </div>
+            @endif
+            <div class="container" style="padding: 100px">
                 @foreach($laws as $law)
-                    <h1 class="display-8">{{ $law->title }} @if($law->has_seen == 0)<span class="badge badge-danger"> New</span>@endif</h1>
+                    <h1 class="display-8" style="text-align: justify">{{ $law->title }} @if($law->has_seen == 0)<span class="badge badge-danger"> New</span>@endif</h1>
                     <a href="#" class="badge badge-dark">{{$law->date}}</a>
                     <span class="badge badge-info">{{$law->country}}</span>
-                    <p class="lead"><?=str_replace('\r\n', '', $law->summarize)?></p>
-                    <a href="document/{{$law->id}}" class="btn btn-primary">More</a>
                     <br>
+                    <br>
+                    <p class="lead" style="text-align: justify"><?=str_replace('\r\n', '', $law->summarize)?></p>
+                    <a href="document/{{$law->id}}" class="btn btn-primary pull-right">More <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                    <br>
+                    <br>
+                    <br>
+                    <hr>
                     <br>
                     <br>
                     <br>
